8e00fcf5959b2c4ba2dffdb7d13d4bbf27e92cbc
Feddy's Thinkpad <cavaliere12.web@gmail.com>
2024-05-10 09:23:44 +0200

    all in one endpoint

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const e = require("./events.js")
 const express = require('express')
 
 const app = express()
-const root = process.env["ROOT"]
+const root = process.env["ROOT"] || ""
 
-app.get(root+'/artist/:artist', (req, res) => {
-    console.log(req.params)
+app.get(root+'/:artist', (req, res) => {
     e.getEvents(req.params.artist)
     .then(async events => {
         if (events.length == 0) res.statusCode(404)
-        res.send(events)
+        res.send(await Promise.all(events.map( async event => ({
+            ...event,
+            dates: await e.getTickets(req.params.artist, event.uri)
+        }))))
     })
     .catch(err => {
         res.status(400).send(err)
     })
 })
 
-app.get(root+'/event/:event', (req, res) => {
-    e.getTickets(req.params.event)
+app.get(root+'/:artist/:event', (req, res) => {
+    e.getTickets(req.params.artist, req.params.event)
     .then(dates => {
         if (dates.length == 0) res.statusCode(404)
         res.send(dates)
